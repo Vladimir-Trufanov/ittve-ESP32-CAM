16ee7ec9f1d624dadcdb8212aae8ba1eca74d57d
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2024-06-11 12:16:26 +0300

    esp32-http-server-v-rezhime-wifi-station-sta - компиляция

--- 94-Передача данных на сайт/esp32-sobstvennaya-set-wi-fi/esp32-sobstvennaya-set-wi-fi.ino
+++ 94-Передача данных на сайт/esp32-sobstvennaya-set-wi-fi/esp32-sobstvennaya-set-wi-fi.ino
@@ -3,21 +3,26 @@
 // Этот пример демонстрирует, как превратить ESP32 в точку доступа (AP) 
 // и отдавать веб-страницы для любого подключенного клиента.
 
+// Подключаем библиотеку специальных методов для работы ESP32 с сетью WiFi
 #include <WiFi.h>
+// Подключаем библиотеку методов для создания и настройки сервера и обработки 
+// HTTP-запросов, чтобы не беспокоится о деталях реализации на низком уровне
 #include <WebServer.h>
 
-/* Устанавливаем свои SSID и пароль */
+// Устанавливаем SSID и пароль создаваемой WiFi - сети
 const char* ssid = "ESP32";  
 const char* password = "01234567";  
+// Выполняем настройки IP адреса для точки доступа
+IPAddress local_ip(192,168,2,1);  // IP-адрес
+IPAddress gateway(192,168,2,1);   // маска IP-подсети
+IPAddress subnet(255,255,255,0);  // IP-шлюз
+// Строим объект WebServer и указываем порт для прослушивания сервером
+// (80 - порт по умолчанию для HTTP), 
+// теперь можно получить доступ к серверу без необходимости указания порта в URL-адресе
+WebServer server(80);
 
 #define LampOn  true    // "лампочка включена"
 #define LampOff false   // "лампочка выключена"
-
-/* Настройки IP адреса */
-IPAddress local_ip(192,168,2,1);
-IPAddress gateway(192,168,2,1);
-IPAddress subnet(255,255,255,0);
-WebServer server(80);
 
 // Инициируем пин и состояние вспышки 4 - прямая логика
 uint8_t LED4pin = 4;
@@ -31,19 +36,34 @@ void setup()
   Serial.begin(115200);
   pinMode(LED4pin, OUTPUT);
   pinMode(LED33pin, OUTPUT);
+  // Настраиваем программную точку доступа для создания сети Wi-Fi, 
+  // проверяя SSID, пароль, IP-адрес, маску IP-подсети и IP-шлюз.
   WiFi.softAP(ssid, password);
   WiFi.softAPConfig(local_ip, gateway, subnet);
+
   delay(100);
+
+  // Привязываем к конкретным функциям обработку входящих HTTP-запросов 
+  // по относительным путям с помощью метода on (первый параметр - это URL-путь, 
+  // а второй - имя функции, которую мы хотим выполнить при запросе этого URL-адреса)
   server.on("/", handle_OnConnect);
   server.on("/led4on", handle_led4on);
   server.on("/led4off", handle_led4off);
   server.on("/led33on", handle_led33on);
   server.on("/led33off", handle_led33off);
+  // Привязываем обработчик на все другие URL-запросы, отличные от указанных в server.on() 
+  // (обработчик должен ответить HTTP-статусом 404 (Not Found) и сообщением для пользователя. 
+  // Для этого формируем функцию server.onNotFound(), которая запустится когда сервер 
+  // получит запрос с URI, который не был указан в server.on
   server.onNotFound(handle_NotFound);
+  
   server.begin();
   Serial.println("HTTP server started");
 }
 
+// Основной цикл loop(). Для обработки входящих HTTP-запросы, постоянно
+// вызывать handleClient() метод объекта сервера, и менять состояния светодиодов
+// по соответствующим запросам
 void loop() 
 {
   server.handleClient();
@@ -55,11 +75,18 @@ void loop()
   else {digitalWrite(LED33pin, HIGH);}
 }
 
+// Выполнить действия, соответствующие корневому (/) URL с помощью server.on. В начале 
+// установить состояние обоих светодиодов на LOW (исходное состояние светодиодов) и вывести
+// их состояние в последовательный монитор. 
 void handle_OnConnect() 
 {
   LED4status = LampOff;
   LED33status = LampOff;
   Serial.println("GPIO4 Status: OFF | GPIO33 Status: OFF");
+  // Для ответа на HTTP-запрос, используем метод send, отправляя код 200 (один из кодов состояния HTTP), 
+  // который соответствует ответу OK - ответ получен и, указывая тип содержимого как «текст/html» и, 
+  // наконец, вызываем пользовательскую функцию SendHTML(), которая создает динамическую HTML-страницу, 
+  // содержащую состояние светодиодов
   server.send(200, "text/html", SendHTML(LED4status,LED33status)); 
 }
 
@@ -98,6 +125,10 @@ void handle_NotFound()
   server.send(404, "text/plain", "Not found");
 }
 
+// Формируем HTML-отображение веб-страницы по каждому запросу, то есть 
+// когда веб-сервер ESP32 получает запрос от веб-клиента. Просто объединяем HTML-код
+// в большую строку и возвращает server.send() функцию.
+// Функция SendHTML принимает состояние светодиодов в качестве параметров для динамической генерации содержимого HTML.
 String SendHTML(uint8_t led4stat,uint8_t led33stat)
 {
   String ptr = "<!DOCTYPE html> <html>\n";
