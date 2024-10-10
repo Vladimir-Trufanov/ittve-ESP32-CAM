d78cf2e5ad1bf1b9e1ca6e336bb57b3db6593e38
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2024-10-10 20:14:38 +0300

    Проба исключений

--- 05-zastavlyaem-esp32-cam-migat-lampochkami/BlinkOnCam32/BlinkOnCam32.ino
+++ 05-zastavlyaem-esp32-cam-migat-lampochkami/BlinkOnCam32/BlinkOnCam32.ino
@@ -23,6 +23,7 @@
 // Менеджер плат:         esp32 by Espressif Systems 3.0.1 installed
 // Платы в данном пакете: ESP32 Dev Board, ESP32-S2 Dev Board, ESP32-S3 Dev Board, ESP32-C3 Dev Board, Arduino Nano ESP32.
 
+#include<Arduino.h>
 #include "tveEsp32Cam10.h"       
 
 void setup() 
