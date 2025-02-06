6d82bafb2ca73cc06980393f95cd89f7bf58c12b
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2025-02-06 15:23:54 +0300

    ESP32-devcam-tve последний вариант

--- 18-potokovaya-peredacha-video-cherez-brauzer-i-rtsp/ESP32-devcam-tve/ESP32-devcam-tve.ino
+++ 18-potokovaya-peredacha-video-cherez-brauzer-i-rtsp/ESP32-devcam-tve/ESP32-devcam-tve.ino
@@ -1,3 +1,17 @@
+
+// Потоковая передача в веб-браузер          ESP32-devcam-tve.ino
+
+/**
+ * Это довольно простой способ транслировать видео с камеры Wi-Fi. По сути, 
+ * ESP32 запускает веб-сервер и при подключении веб-браузера отправляет 
+ * изображения с камеры в веб-браузер. Таким образом, это не настоящий поток 
+ * данных с камеры, а скорее поток изображений.
+ * 
+ * Для включения потоковой передачи в веб-браузер используется веб-сервер 
+ * из фреймворка Arduino ESP32.
+ * 
+**/
+
 #include "OV2640.h"
 #include <WiFi.h>
 #include <WebServer.h>
@@ -7,221 +21,130 @@
 #include "OV2640Streamer.h"
 #include "CRtspSession.h"
 
-//#define ENABLE_OLED //if want use oled ,turn on thi macro
-
-// #define SOFTAP_MODE // If you want to run our own softap turn this on
 #define ENABLE_WEBSERVER
-//#define ENABLE_RTSPSERVER
-
-#ifdef ENABLE_OLED
-#include "SSD1306.h"
-#define OLED_ADDRESS 0x3c
-#define I2C_SDA 14
-#define I2C_SCL 13
-SSD1306Wire display(OLED_ADDRESS, I2C_SDA, I2C_SCL, GEOMETRY_128_32);
-bool hasDisplay; // we probe for the device at runtime
-#endif
 
 OV2640 cam;
 
-#ifdef ENABLE_WEBSERVER
+// Инициализируем веб-сервер для обработки входящих HTTP-запросов на порту 80
 WebServer server(80);
-#endif
 
-#ifdef ENABLE_RTSPSERVER
-WiFiServer rtspServer(8554);
-#endif
+//const char* ssid     = "OPPO A9 2020";
+//const char* password = "b277a4ee84e8";
 
+const char* ssid     = "TP-Link_B394";
+const char* password = "18009217";
 
-#ifdef SOFTAP_MODE
-   IPAddress apIP = IPAddress(192, 168, 1, 1);
-#else
-   //#include "wifikeys.h"
-   const char* ssid     = "OPPO A9 2020";
-   const char* password = "b277a4ee84e8";
-#endif
-
-#ifdef ENABLE_WEBSERVER
+// ****************************************************************************
+// *   Для непрерывной потоковой передачи изображений выполнить две задачи:   *
+// *      #1 - сообщить веб-браузеру о данных, которые будут отправляться;    *
+// *      #2 - отправлять изображения с камеры в цикле до тех пор, пока       *
+// *           веб-браузер не отключится                                      *
+// ****************************************************************************
+int ii=0;
 void handle_jpg_stream(void)
 {
-    WiFiClient client = server.client();
-    String response = "HTTP/1.1 200 OK\r\n";
-    response += "Content-Type: multipart/x-mixed-replace; boundary=frame\r\n\r\n";
-    server.sendContent(response);
+   // Обрабатываем запросы веб-потока: выдаём первый ответ для подготовки потоковой передачи,
+   // затем запускаемся в цикле для обновления веб-контента каждый раз, когда становится 
+   // доступен новый кадр
+   WiFiClient client = server.client();
+   String response = "HTTP/1.1 200 OK\r\n";
+   response += "Content-Type: multipart/x-mixed-replace; boundary=frame\r\n\r\n";
+   server.sendContent(response);
+   while (1)
+   {
+      ii++;
+      Serial.print("ii: "); Serial.println(ii);
+      cam.run();
+      if (!client.connected()) break;
+      response = "--frame\r\n";
+      response += "Content-Type: image/jpeg\r\n\r\n";
+      server.sendContent(response);
+      delay(500);
 
-    while (1)
-    {
-        cam.run();
-        if (!client.connected())
-            break;
-        response = "--frame\r\n";
-        response += "Content-Type: image/jpeg\r\n\r\n";
-        server.sendContent(response);
-
-        client.write((char *)cam.getfb(), cam.getSize());
-        server.sendContent("\r\n");
-        if (!client.connected())
-            break;
-    }
+      client.write((char *)cam.getfb(), cam.getSize());
+      Serial.println("1-r-n");
+      server.sendContent("\r\n");
+      Serial.println("2-r-n");
+      // delay(150); // 2025-02-03
+      if (!client.connected())
+      { 
+         Serial.println("!client.connected()");
+         break;
+      }
+      Serial.println("3-r-n");
+   }
 }
-
+// ****************************************************************************
+// *    Обработать запросы на отдельные изображения: считать изображение с    *
+// *               камеры и отправить его в веб-браузер.                      *
+// ****************************************************************************
 void handle_jpg(void)
 {
-    WiFiClient client = server.client();
-
-    cam.run();
-    if (!client.connected())
-    {
-        return;
-    }
-    String response = "HTTP/1.1 200 OK\r\n";
-    response += "Content-disposition: inline; filename=capture.jpg\r\n";
-    response += "Content-type: image/jpeg\r\n\r\n";
-    server.sendContent(response);
-    client.write((char *)cam.getfb(), cam.getSize());
+   WiFiClient client = server.client();
+   cam.run();
+   if (!client.connected()) return;
+   String response = "HTTP/1.1 200 OK\r\n";
+   response += "Content-disposition: inline; filename=capture.jpg\r\n";
+   response += "Content-type: image/jpeg\r\n\r\n";
+   server.sendContent(response);
+   client.write((char *)cam.getfb(), cam.getSize());
 }
+// ****************************************************************************
+// *       Обработать все остальные запросы - отправить простой ответ         *
+// *               с инструкциями обратно в веб-браузер                       *
+// ****************************************************************************
 
 void handleNotFound()
 {
-    String message = "Server is running!\n\n";
-    message += "URI: ";
-    message += server.uri();
-    message += "\nMethod: ";
-    message += (server.method() == HTTP_GET) ? "GET" : "POST";
-    message += "\nArguments: ";
-    message += server.args();
-    message += "\n";
-    server.send(200, "text/plain", message);
+   String message = "Server is running!\n\n";
+   message += "URI: ";
+   message += server.uri();
+   message += "\nMethod: ";
+   message += (server.method() == HTTP_GET) ? "GET" : "POST";
+   message += "\nArguments: ";
+   message += server.args();
+   message += "\n";
+   server.send(200, "text/plain", message);
 }
-#endif
-
-#ifdef ENABLE_OLED
-#define LCD_MESSAGE(msg) lcdMessage(msg)
-#else
-#define LCD_MESSAGE(msg)
-#endif
-
-#ifdef ENABLE_OLED
-void lcdMessage(String msg)
-{
-    if(hasDisplay) {
-        display.clear();
-        display.drawString(128 / 2, 32 / 2, msg);
-        display.display();
-    }
-}
-#endif
 
 CStreamer *streamer;
 
 void setup()
 {
-  #ifdef ENABLE_OLED
-    hasDisplay = display.init();
-    if(hasDisplay) {
-        display.flipScreenVertically();
-        display.setFont(ArialMT_Plain_16);
-        display.setTextAlignment(TEXT_ALIGN_CENTER);
-    }
-  #endif
-    LCD_MESSAGE("booting");
-
-    Serial.begin(115200);
-    while (!Serial)
-    {
-        ;
-    }
-    //cam.init(esp32cam_config);
-    cam.init(esp32cam_aithinker_config);
+   Serial.begin(115200);
+   while (!Serial) {;}
+   cam.init(esp32cam_aithinker_config);
     
-    IPAddress ip;
+   IPAddress ip;
 
+   WiFi.mode(WIFI_STA);
+   WiFi.begin(ssid, password);
+   while (WiFi.status() != WL_CONNECTED)
+   {
+      delay(500);
+      Serial.print(F("."));
+   }
+   ip = WiFi.localIP();
+   Serial.println(F("WiFi connected"));
+   Serial.println("");
+   Serial.println(ip);
 
-#ifdef SOFTAP_MODE
-    const char *hostname = "devcam";
-    // WiFi.hostname(hostname); // FIXME - find out why undefined
-    LCD_MESSAGE("starting softAP");
-    WiFi.mode(WIFI_AP);
-    WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
-    bool result = WiFi.softAP(hostname, "12345678", 1, 0);
-    if (!result)
-    {
-        Serial.println("AP Config failed.");
-        return;
-    }
-    else
-    {
-        Serial.println("AP Config Success.");
-        Serial.print("AP MAC: ");
-        Serial.println(WiFi.softAPmacAddress());
+   // Определяем действия при различных HTTP-запросах. Простой запрос http://<IP-АДРЕС>/ 
+   // запускает непрерывную потоковую передачу изображений в веб-браузер.
+   // http://<IP-АДРЕС>/jpg отправляет одно изображение с камеры в веб-браузер.
+   // Все остальные запросы игнорируются.
 
-        ip = WiFi.softAPIP();
-    }
-#else
-    LCD_MESSAGE(String("join ") + ssid);
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(500);
-        Serial.print(F("."));
-    }
-    ip = WiFi.localIP();
-    Serial.println(F("WiFi connected"));
-    Serial.println("");
-    Serial.println(ip);
-#endif
-
-    LCD_MESSAGE(ip.toString());
-
-#ifdef ENABLE_WEBSERVER
-    server.on("/", HTTP_GET, handle_jpg_stream);
-    server.on("/jpg", HTTP_GET, handle_jpg);
-    server.onNotFound(handleNotFound);
-    server.begin();
-#endif
-
-#ifdef ENABLE_RTSPSERVER
-    rtspServer.begin();
-
-    //streamer = new SimStreamer(true);             // our streamer for UDP/TCP based RTP transport
-    streamer = new OV2640Streamer(cam);             // our streamer for UDP/TCP based RTP transport
-#endif
+   // Устанавливаем функцию для обработки потоковых запросов
+	server.on("/", HTTP_GET, handle_jpg_stream);
+   // Устанавливаем функцию для обработки запросов на отдельные изображения
+	server.on("/jpg", HTTP_GET, handle_jpg);
+   // Устанавливаем функцию для обработки других запросов
+	server.onNotFound(handleNotFound);
+   // Запускаем веб-сервер
+   server.begin();
 }
 
 void loop()
 {
-#ifdef ENABLE_WEBSERVER
-    server.handleClient();
-#endif
-
-#ifdef ENABLE_RTSPSERVER
-    uint32_t msecPerFrame = 100;
-    static uint32_t lastimage = millis();
-
-    // If we have an active client connection, just service that until gone
-    streamer->handleRequests(0); // we don't use a timeout here,
-    // instead we send only if we have new enough frames
-    uint32_t now = millis();
-    if(streamer->anySessions()) {
-        if(now > lastimage + msecPerFrame || now < lastimage) { // handle clock rollover
-            streamer->streamImage(now);
-            lastimage = now;
-
-            // check if we are overrunning our max frame rate
-            now = millis();
-            if(now > lastimage + msecPerFrame) {
-                printf("warning exceeding max frame rate of %d ms\n", now - lastimage);
-            }
-        }
-    }
-    
-    WiFiClient rtspClient = rtspServer.accept();
-    if(rtspClient) {
-        Serial.print("client: ");
-        Serial.print(rtspClient.remoteIP());
-        Serial.println();
-        streamer->addSession(rtspClient);
-    }
-#endif
+   server.handleClient();
 }
--- 18-potokovaya-peredacha-video-cherez-brauzer-i-rtsp/ESP32-devcam-tve/wifikeys.h
+++
@@ -1,2 +0,0 @@
-const char* ssid     = "OPPO A9 2020";
-const char* password = "b277a4ee84e8";
