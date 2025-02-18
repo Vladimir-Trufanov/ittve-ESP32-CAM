fdc8cb87aebbd535fb63075d5a002e6078b453d6
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2025-02-18 11:10:06 +0300

    Echo без массивов работает

--- 18-potokovaya-peredacha-video-cherez-brauzer-i-rtsp/StreamESP32/Rus32CAMExt/Rus32CAMExt.ino
+++ 18-potokovaya-peredacha-video-cherez-brauzer-i-rtsp/StreamESP32/Rus32CAMExt/Rus32CAMExt.ino
@@ -16,115 +16,24 @@
 #include "Echo.h"   
 
 // ESP32-CAM doesn't have dedicated i2c pins, so we define our own. Let's choose 15 and 14
+// Инициируем ведение журнала на Oled-дисплее
 #define I2C_SDA 14
 #define I2C_SCL 13
-
-//TwoWire I2Cbus = TwoWire(0);
-
-// Display defines
-/*
-#define SCREEN_WIDTH    128
-#define SCREEN_HEIGHT   64
-#define OLED_RESET      -1
-#define SCREEN_ADDRESS  0x3C
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &I2Cbus, OLED_RESET);
-*/
-
-TEcho echo(14,13,0x3C); 
-
-/*
-void displine(String iline, int point=0)
-{
-  //display.clearDisplay();
-  display.setCursor(0, point);
-  //display.println(utf8rus("012345678901\nпривет\n012345678901\nмир"));
-  String line=utf8rus(iline);
-  line=line.substring(0,10);
-  display.println("          ");
- display.setCursor(0, point);
-  display.println(line);
-  display.display();
-  //delay(2000);
-}
-*/
-
+TEcho echo(I2C_SDA,I2C_SCL,0x3C); 
 
 void setup()
 {
   Serial.begin(115200);
-
-  // Initialize I2C with our defined pins
-  //I2Cbus.begin(I2C_SDA, I2C_SCL, 100000);
-  
-  // Инициируем ведение журнала на Oled-дисплее
-  
-  //bool OledSuccess = echo.Init();
   Serial.println("Initialize display1");
-  //echo.Init();
   echo.out("Init!");
   echo.out("Init!");
   echo.out("Init!");
   Serial.println("Initialize display2");
-  /*
-
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
-  {
-    Serial.printf("SSD1306 OLED display failed to initalize.\nCheck that display SDA is connected to pin %d and SCL connected to pin %d\n", I2C_SDA, I2C_SCL);
-    while (true);
-  }
-  
-  delay(2000);
-  display.cp437(true);
-
-  Serial.println("Show 'Hello World!' on display");
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.setTextSize(2);
-  display.setTextColor(SSD1306_WHITE);
-*/
-
-  /*
-  display.println(utf8rus("старт"));
-  display.display();
-  delay(2000);
-
-  //display.clearDisplay();
-  display.setCursor(0, 16);
-  //display.println(utf8rus("012345678901\nпривет\n012345678901\nмир"));
-  display.println(utf8rus("01234567890"));
-  display.display();
-  delay(2000);
-
-  //display.clearDisplay();
-  display.setCursor(0, 32);
-  //display.println(utf8rus("012345678901\nпривет\n012345678901\nмир"));
-  display.println(utf8rus("01234567890"));
-  display.display();
-  delay(2000);
-
-  //display.clearDisplay();
-  display.setCursor(0, 48);
-  //display.println(utf8rus("012345678901\nпривет\n012345678901\nмир"));
-  display.println(utf8rus("01234567890"));
-  display.display();
-  //delay(2000);
-*/
-  /*
-  displine("01234567890",0);
-  displine("вторая",16);
-  displine("ЫВАПдесять",32);
-  displine("одиннадцать",48);
-  */
-  
-
 }
 
 void loop()
 {
 }
-
-
 
 /* Функция перекодировки русских букв из UTF-8 в Win-1251 */
 String utf8rus(String source)
@@ -156,7 +65,6 @@ String utf8rus(String source)
   }
 return target;
 }
-
 
 // ******************************************************** Rus32CAMExt.ino ***
 
