0575eb16c2baaaed229d2e34b2d8a07fd4e3cddb
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2025-12-17 10:26:33 +0300

    Проверка работоспособности для v 3.3.4

--- 18-potokovaya-peredacha-video-cherez-brauzer-i-rtsp/StreamESP32/StreamESP32.ino
+++ 18-potokovaya-peredacha-video-cherez-brauzer-i-rtsp/StreamESP32/StreamESP32.ino
@@ -46,11 +46,11 @@ int pictureNumber = 0;
   bool hasDisplay; // мы проверяем наличие устройства во время выполнения
 #endif
 
-const char* ssid     = "OPPO A9 2020";
-const char* password = "b277a4ee84e8";
+//const char* ssid     = "OPPO A9 2020";
+//const char* password = "b277a4ee84e8";
 
-//const char* ssid     = "TP-Link_B394";
-//const char* password = "18009217";
+const char* ssid     = "TP-Link_B394";
+const char* password = "18009217";
 
 OV2640 cam;
 
